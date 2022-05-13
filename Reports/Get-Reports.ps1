1f12de41c6aca7c8f5f64057479454907e35733f
donbood <95503083+donbood@users.noreply.github.com>
2022-05-13 08:58:18 -0400

    Update Get-Reports.ps1

--- Reports/Get-Reports.ps1
+++ Reports/Get-Reports.ps1
@@ -1,4 +1,16 @@
-$jsonFromPython = python .\fetchReports.py
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [ValidateSet("access_transparency", "admin", "calendar", "chat", "chrome", "context_aware_access", "data_studio", "drive", "gcp", "gplus", "groups", "groups_enterprise", "jamboard", "keep", "login", "meet", "mobile", "rules", "saml", "token", "user_accounts")]
+    $LogType
+)
+
+if ($PSBoundParameters.Containskey("LogType")) {
+    $jsonFromPython = python .\fetchReports.py $LogType
+}
+else {
+    $jsonFromPython = python .\fetchReports.py admin
+}
 
 if ($jsonFromPython -eq "No logs found.") {
     Write-Host "No logs found."
@@ -6,40 +18,41 @@ if ($jsonFromPython -eq "No logs found.") {
 else {
     $Reports = $jsonFromPython | ConvertFrom-Json
     $outputArray = @()
-    
-}
 
-foreach ($report in $Reports) {
-    $eventDescArray = @()
-    $events = $report.events
-    foreach ($event in $events) {
+    
+    foreach ($report in $Reports) {
+        $eventDescArray = @()
+        $events = $report.events
+        foreach ($event in $events) {
         
-        $EventParams = $event.parameters
+            $EventParams = $event.parameters
 
-        $paramArray = @()
-        foreach ($eventParam in $EventParams) {
-            $paramObj = [pscustomobject] @{
-                paramName  = $eventParam.name
-                paramValue = $eventParam.value
+            $paramArray = @()
+            foreach ($eventParam in $EventParams) {
+                $paramObj = [pscustomobject] @{
+                    paramName  = $eventParam.name
+                    paramValue = $eventParam.value
+                }
+                $paramArray += $paramObj
             }
-            $paramArray += $paramObj
-        }
 
-        foreach ($paramObj in $paramArray) {
-            $strang = "$($ParamObj.ParamName): $($ParamObj.ParamValue)"
-            $eventDescArray += $strang
-        }
+            foreach ($paramObj in $paramArray) {
+                $strang = "$($ParamObj.ParamName): $($ParamObj.ParamValue)"
+                $eventDescArray += $strang
+            }
 
     
+        }
+        $eventObj = [pscustomobject] @{
+            Actor = $report.actor.email
+            Date  = $report.id.time
+            Event = $report.events.name
+            Type  = $report.events.type
+            Desc  = $eventDescArray -join "|"
+        }
+        $outputArray += $eventObj
     }
-    $eventObj = [pscustomobject] @{
-        Actor = $report.actor.email
-        Date  = $report.id.time
-        Event = $report.events.name
-        Type  = $report.events.type
-        Desc  = $eventDescArray -join "|"
-    }
-    $outputArray += $eventObj
-}
 
-$outputArray | Export-Csv .\AdminReportLog.csv -NoTypeInformation
+    $outputArray | Export-Csv .\AdminReportLog.csv -NoTypeInformation
+    $outputArray
+}
