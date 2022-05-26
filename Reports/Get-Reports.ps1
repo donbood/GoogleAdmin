97fb375c7cb8e30efac6f292dbdaf003c752bb46
donbood <95503083+donbood@users.noreply.github.com>
2022-05-26 11:57:38 -0400

    Update Get-Reports.ps1
    Updated for improved usability.

--- Reports/Get-Reports.ps1
+++ Reports/Get-Reports.ps1
@@ -1,58 +1,76 @@
-[CmdletBinding()]
-param (
-    [Parameter()]
-    [ValidateSet("access_transparency", "admin", "calendar", "chat", "chrome", "context_aware_access", "data_studio", "drive", "gcp", "gplus", "groups", "groups_enterprise", "jamboard", "keep", "login", "meet", "mobile", "rules", "saml", "token", "user_accounts")]
-    $LogType
-)
+function Get-Reports {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [ValidateSet("access_transparency", "admin", "calendar", "chat", "chrome", "context_aware_access", "data_studio", "drive", "gcp", "gplus", "groups", "groups_enterprise", "jamboard", "keep", "login", "meet", "mobile", "rules", "saml", "token", "user_accounts")]
+        $LogType
+    )
 
-if ($PSBoundParameters.Containskey("LogType")) {
-    $jsonFromPython = python .\fetchReports.py $LogType
-}
-else {
-    $jsonFromPython = python .\fetchReports.py admin
-}
+    $Reports = ""
 
-if ($jsonFromPython -eq "No logs found.") {
-    Write-Host "No logs found."
-}
-else {
-    $Reports = $jsonFromPython | ConvertFrom-Json
-    $outputArray = @()
+    if ($PSBoundParameters.Containskey("LogType")) {
+        $jsonFromPython = python .\fetchReports.py $LogType
 
-    
-    foreach ($report in $Reports) {
-        $eventDescArray = @()
-        $events = $report.events
-        foreach ($event in $events) {
-        
-            $EventParams = $event.parameters
-
-            $paramArray = @()
-            foreach ($eventParam in $EventParams) {
-                $paramObj = [pscustomobject] @{
-                    paramName  = $eventParam.name
-                    paramValue = $eventParam.value
-                }
-                $paramArray += $paramObj
-            }
-
-            foreach ($paramObj in $paramArray) {
-                $strang = "$($ParamObj.ParamName): $($ParamObj.ParamValue)"
-                $eventDescArray += $strang
-            }
-
-    
+        if ($jsonFromPython -eq "No logs found.") {
+            Write-Host "No logs found."
         }
-        $eventObj = [pscustomobject] @{
-            Actor = $report.actor.email
-            Date  = $report.id.time
-            Event = $report.events.name
-            Type  = $report.events.type
-            Desc  = $eventDescArray -join "|"
+        else {
+            $Reports = $jsonFromPython | ConvertFrom-Json
         }
-        $outputArray += $eventObj
+    }
+    else {
+        $jsonFromPython = python .\fetchReports.py admin
+
+        if ($jsonFromPython -eq "No logs found.") {
+            Write-Host "No logs found."
+        }
+        else {
+            $Reports = $jsonFromPython | ConvertFrom-Json
+        }
     }
 
-    $outputArray | Export-Csv .\AdminReportLog.csv -NoTypeInformation
-    $outputArray
+    if ($Reports -ne "") {
+        $outputArray = @()
+    
+        
+        foreach ($report in $Reports) {
+            $eventDescArray = @()
+            $events = $report.events
+            foreach ($event in $events) {
+            
+                $EventParams = $event.parameters
+    
+                $paramArray = @()
+                foreach ($eventParam in $EventParams) {
+                    $paramObj = [pscustomobject] @{
+                        paramName  = $eventParam.name
+                        paramValue = $eventParam.value
+                    }
+                    $paramArray += $paramObj
+                }
+    
+                foreach ($paramObj in $paramArray) {
+                    $strang = "$($ParamObj.ParamName): $($ParamObj.ParamValue)"
+                    $eventDescArray += $strang
+                }
+    
+        
+            }
+
+            $time = $report.id.time
+            $datetime = ([DateTime]$time).ToUniversalTime()
+            $EST = [TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($datetime, 'Eastern Standard Time')
+            $eventObj = [pscustomobject] @{
+                Date  = $EST.toString()
+                Actor = $report.actor.email
+                Event = $report.events.name
+                Type  = $report.events.type
+                Desc  = $eventDescArray -join "|"
+            }
+            $outputArray += $eventObj
+        }
+    
+        $outputArray | Export-Csv ".\AdminReportLog-$($LogType).csv" -NoTypeInformation
+        $outputArray
+    }
 }
