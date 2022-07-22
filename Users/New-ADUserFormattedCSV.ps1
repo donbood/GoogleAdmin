6c8279e605cfcb8167ba57a66d18ef3d44aad633
donbood <95503083+donbood@users.noreply.github.com>
2022-07-22 15:54:46 -0400

    Create New-ADUserFormattedCSV.ps1

---
+++ Users/New-ADUserFormattedCSV.ps1
@@ -0,0 +1,35 @@
+$users = .\Get-Users.ps1
+
+function format-OU {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        $ou
+    )
+
+    $base = "DC=aacs,DC=local"
+
+    if($ou.Length -gt 1) {
+        $array = $ou -split '/'
+        [array]::reverse($array)
+        $output = @()
+        $output += ($array | Select-Object -SkipLast 1) -replace '^.*$', 'OU=$0'
+        $output = $output -join ','
+        $output += ",$($base)"
+    } elseif ($ou -eq "/") {
+        $output = $base
+    }
+
+    return $output
+}
+
+$users | 
+select-object *, 
+@{N ="Name";E = {"$($_.firstname) $($_.lastName)"}}, 
+@{N ="GivenName";E = {"$($_.firstName)"}}, 
+@{N ="Surname";E = {"$($_.lastName)"}},
+@{N ="SamAccountName"; E = {$($_.email) -replace ".{13}$"}},
+@{N = "Path"; E = {format-OU -ou $_.orgUnit}},
+@{N = "EmailAddress"; E = {$($_.email)}} | 
+select-object -Property Name,GivenName,Surname,SamAccountName,Path,Email | 
+Export-Csv ".\NewADUsers.csv" -NoTypeInformation
